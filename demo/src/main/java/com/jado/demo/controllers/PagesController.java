8ace45c63de2a016de86f6db7dc95ce141400a38
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-09-16 23:24:28 +0200

    création de PagesControlle pour retourner les vues

--- demo/src/main/java/com/jado/demo/controllers/PagesController.java
+++ demo/src/main/java/com/jado/demo/controllers/PagesController.java
@@ -1,11 +1,35 @@
 package com.jado.demo.controllers;
 
+//import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PagesController {
 
-	   public String home() {
-		   return "Hello Word";
+	@GetMapping("/")
+	// @ResponseBody quand on veut interpreter le text comme tel sans vue
+	   //public String home(HttpServletRequest request, ModelMap modelmap) {
+public String home(@RequestParam(required=false, defaultValue="Cher(e) Donateur(trice)")
+		String name, ModelMap modelmap) {
+		
+		//un ternaire pour véfier si le paarametre a bien été passé, JEE
+		
+		/*String name = request.getParameter("name") != null && !request.getParameter("name").isEmpty()?
+				request.getParameter("name") : "World";*/
+				
+				/*
+				 * Pour passer un paramètre à la vue on injecte le modelmap
+				 * */	
+		//System.out.println("\n\n\n" + request.getParameter("name") + "\n\n\n");
+	
+		   //return "Hello World";//ici il va cherche une vue Hello world
+			modelmap.put("name", name.toUpperCase());
+			
+		return "pages/home";// on cherche le vue home dans templates/pages
 	   }
 }
